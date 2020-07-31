c69f4d77a1345f22e2b9ae0f798ac8ae022c8fdc
Mihai Anghel <mihai.anghel@lpkf.com>
2020-07-31 08:00:23 +0200

    this is the first commit

---
+++ GIT_Test_App/GIT_Test_App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GIT_Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test to play with GIT");
+
+            Student std = new Student();
+            std.Greetings("Mihai","Anghel");
+
+            Console.WriteLine();
+        }
+    }
+}
