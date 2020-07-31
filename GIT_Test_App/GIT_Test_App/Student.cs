196db12033ab7a9f6cb45d68c98c1a40dc759c42
Mihai Anghel <mihai.anghel@lpkf.com>
2020-07-31 08:02:27 +0200

    This is the second commit

---
+++ GIT_Test_App/GIT_Test_App/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GIT_Test_App
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Greetings(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+
+            Console.WriteLine("My name is {0} - {0}", firstName, lastName);
+        }
+
+    }
+}
