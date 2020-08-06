2b7b8c74e85630a70fab9d09e1bc68183677a39e
Mihai Anghel <mihai.anghel@lpkf.com>
2020-08-06 14:35:30 +0200

    Testcommit Buildfiles added

--- GIT_Test_App/GIT_Test_App/Student.cs
+++ GIT_Test_App/GIT_Test_App/Student.cs
@@ -9,14 +9,16 @@ namespace GIT_Test_App
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string NickName { get; set; }
+        public string Address { get; set; }
 
-        public void Greetings(string firstName, string lastName, string nickName)
+        public void Greetings(string firstName, string lastName, string nickName, string address)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
             this.NickName = nickName;
+            this.Address = address;
 
-            Console.WriteLine("My name is {0} - {1}, and my nick name is: {2}", firstName, lastName, nickName);
+            Console.WriteLine("My name is {0} - {1}, and my nick name is: {2} and I am living on the {3} street", firstName, lastName, nickName, address);
         }
 
     }
